fdda12985377011e27c227cbc2c243b9944939aa
Jack Huang <jack.huang@stanford.edu>
2013-10-24 12:14:40 -0700

    add mreduce6.cl

--- reduce.c
+++ reduce.c
@@ -6,238 +6,192 @@
 #include <CL/cl_ext.h>
 #include "oclutil.h"
 
-#include "test/test.h"
-
 #ifndef CL_MEM_BANK_1_ALTERA
 #define CL_MEM_BANK_1_ALTERA 0
 #define CL_MEM_BANK_2_ALTERA 0
 #endif
-
 #define AOCL_ALIGNMENT 64
 
-#define T float
-#define blockSize 128
+#define REPEAT 11
+
+#define MREDUCE6 0x01
+void parse_arguments(int argc, char **argv);
+void mreduce6(void);
 
 
-void getStat(long *times, long *avg, long *std);
+cl_device_id device;
+int kernels;
+size_t size;
+
+
 
 int main(int argc, char **argv) {
-	printf("======== BEGIN test.c ========\n");
+	int i;
+	cl_int ret;
+	printf(">>>>> BEGIN reduce.c <<<<<\n");
 
-	int i, j;
-	cl_int ret, ret0, ret1, ret2, ret3;
+
+	parse_arguments(argc, argv);
+	if(kernels & MREDUCE6) {
+		mreduce6();
+	}
+
+
+
+
+
+
+	printf(">>>>> END   benchmark.c <<<<<\n");
+	exit(0);
+}
+
+/**
+ *	Get OpenCL devices and display device info.
+ *	Parse arguments.
+ */
+void parse_arguments(int argc, char **argv) {
+	int i;
+	cl_int ret;
+
 	cl_uint num_devices;
 	cl_device_id *devices;
-
-	/*
-		Get devices
-	*/
 	ret = oclGetDevices(&num_devices, &devices);
-	if(ret != CL_SUCCESS) {
-		printf("ERROR in oclGetDevices(): %s\n", oclReturnCodeToString(ret));
-		return 1;
+	CHECK_RC(ret, "oclGetDevices()")
+	printf("Get OpenCL devices and display device info\n");
+	printf("\tDevices found: %u\n", num_devices);
+	for(i = 0; i < num_devices; i++) {
+		printf("\tInfo of device %d:\n", i);
+		ret = oclPrintDeviceInfo(devices[i], "\t\t");
+		CHECK_RC(ret, "oclPrintDeviceInfo()")
 	}
 
-	if(argc < 5) {
-		printf("Not enough arguments\n");
-		return -1;
+	// parse arguments
+	int sel = (argc < 2) ? -1 : atoi(argv[1]);
+	if(sel < 0 || sel >= num_devices) {
+		printf("Invalid device selection: %d\n", sel);
+		exit(-1);
 	}
-	
-	
-	/*
-		Parse arguments
-	*/
-	int sel = atoi(argv[1]);
-	char *kernel_file = argv[2];
-	char *kernel_name = argv[3];
-	int form = atoi(argv[4]);
-	int num_data = atoi(argv[5]) << 20;
-	int iter = atoi(argv[6]);
+	device = devices[sel];
+	kernels = (argc < 3) ? 0 : atoi(argv[2]);
+	size = (argc < 4) ? 16 << 20 : 1 << atoi(argv[3]);
+	printf("\tDevice selected: %d\n", sel);
+	printf("\tKernels to run: %d\n", kernels);
+	printf("\tData size: %d\n", size);
 
-	printf("Running Test on device %d:\n", sel);
-	printf("\tKernel file: %s\n", kernel_file);
-	printf("\tKernel: %s\n", kernel_name);
-	pritnf("\tKernel form: %d\n", form);
-	printf("\tData size: %d\n", data_size);
-	printf("\tIterations: %d\n", iter);
-	if(sel >= num_devices) {
-		printf("Device #%d does not exist.\n", sel);
-		return -1;
-	}
+}
 
+void mreduce6(void) {
+	int i, j;
+	cl_int ret;
+	printf("Running mreduce6()\n");
 
 	/*
-		Build OCL kernel
+		Build OpenCL kernel
 	*/
-	printf("Build OCL Kernels\n");
 	cl_context context;
 	cl_command_queue queue;
 	cl_kernel kernel;
 	cl_build_status status;
 	char *log;
-	ret = oclKernelSetup(devices[sel], kernel_file, kernel_name, 
-		&context, &queue, &kernel, &status, &log);
-	if(ret != CL_SUCCESS) {
-		printf("ERROR in oclKernelSetup(): %s\n", oclReturnCodeToString(ret));
-		return -1;
-	} else if(status != CL_BUILD_SUCCESS) {
-		printf("Build status:%s\n", oclBuildStatusToString(status));
-		printf("Build log:\n%s\n", log);
-		return -1;
-	}
-
-	/* 
-		Memory Allocation
-	*/
-	printf("Memory Allocation\n");
-	size_t gsz = num_data;
-	size_t lsz = blockSize;
-	size_t nwg = data_size / lsz;
-	size_t in_data_sz = sizeof(T) * num_data;
-	size_t out_data_sz = sizeof(T) * nwg;
-	T *in_data, *out_data;
-	cl_mem in_data_mem, out_data_mem;
-	printf("\tData Unit Size: %lu\n", sizeof(T));
-	printf("\tGlobal Size: %lu\n", gsz);
-	printf("\tLocal Size: %lu\n", lsz);
-	printf("\tNumber of Workgroups: %lu\n", nwg)
-	printf("\tInput Memory Size: %u \n", (unsigned)in_data_sz);
-	printf("\tOutput Memory Size: %u \n", (unsigned)out_data_sz);
-	
-#ifdef ALTERA		
-	posix_memalign ((void **)&in_data, AOCL_ALIGNMENT, in_data_sz);
-	posix_memalign ((void **)&out_data, AOCL_ALIGNMENT, out_data_sz);	
+#ifndef ALTERA
+	char *kernel_file = "reduce/mreduce6.cl";
 #else
-	in_data = (T *)malloc(in_data_sz);
-	out_data = (T *)malloc(out_data_sz);		
+	char *kernel_file = "reduce/mreduce6.aocx";
 #endif
-	in_data_mem = clCreateBuffer(context, CL_MEM_BANK_1_ALTERA | CL_MEM_READ_ONLY, in_data_sz, NULL, &ret0);
-	out_data_mem = clCreateBuffer(context, CL_MEM_BANK_1_ALTERA | CL_MEM_WRITE_ONLY, out_data_sz, NULL, &ret1);
-	if(ret0 != CL_SUCCESS | ret1 != CL_SUCCESS) {
-		printf("ERROR in clCreateBuffer(): %s, %s\n", oclReturnCodeToString(ret0), oclReturnCodeToString(ret1));
-		return -1;
-	}
+	ret = oclKernelSetup(device, kernel_file, "mreduce6", 
+		&context, &queue, &kernel, &status, &log);
+	CHECK_RC(ret, "oclKernelSetup()")
+	CHECK_BS(status, "oclKernelSetup()", log)
 
 	/*
-		Set Kernel Arguments
+		Initialize Memory
 	*/
-	ret0 = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&in_data_mem);
-	ret1 = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&out_data_mem);
-	ret2 = clSetKernelArg(kernel, 2, sizeof(int), (void *)&num_data);
-	ret3 = clSetKernelArg(kernel, 3, lsz * sizeof(T), NULL);
-	if(ret0 != CL_SUCCESS | ret1 != CL_SUCCESS | ret2 != CL_SUCCESS | ret3 != CL_SUCCESS) {
-		printf("ERROR in clSetKernelArg(): %s, %s, %s, %s\n", oclReturnCodeToString(ret0), oclReturnCodeToString(ret1), oclReturnCodeToString(ret2), oclReturnCodeToString(ret3));
-		return -1;
+	size_t local_sz = 128;
+	size_t workgroups = size / local_sz / 2 / sizeof(float);
+	size_t global_sz = local_sz * workgroups;
+	size_t input_sz = size;
+	size_t output_sz = workgroups * sizeof(float);
+	float *input, *output;
+#ifndef ALTERA
+	input = malloc(input_sz);
+	output = malloc(output_sz);
+#else
+	posix_memalign ((void **)&input, AOCL_ALIGNMENT, input_sz);
+	posix_memalign ((void **)&output, AOCL_ALIGNMENT, output_sz);
+#endif
+	unsigned int input_len = input_sz / sizeof(float);
+	for(i = 0; i < input_len; i++) {
+		input[i] = (float)i;
 	}
+	printf("\tworkgroups:\t%lu\n", workgroups);
+	printf("\tlocal_sz:\t%lu\n", local_sz);
+	printf("\tglobal_sz:\t%lu\n", global_sz);
+	printf("\tinput_sz:\t%lu\n", input_sz);
+	printf("\toutput_sz:\t%lu\n", output_sz);
+	cl_mem input0_mem = clCreateBuffer(context, CL_MEM_BANK_1_ALTERA | CL_MEM_READ_ONLY, input_sz / 2, NULL, &ret);
+	CHECK_RC(ret, "clCreateBuffer(input0_mem)")
+	cl_mem input1_mem = clCreateBuffer(context, CL_MEM_BANK_2_ALTERA | CL_MEM_READ_ONLY, input_sz / 2, NULL, &ret);
+	CHECK_RC(ret, "clCreateBuffer(input1_mem)")
+	cl_mem output_mem = clCreateBuffer(context, CL_MEM_BANK_1_ALTERA | CL_MEM_READ_ONLY, output_sz, NULL, &ret);
+	CHECK_RC(ret, "clCreateBuffer(output_mem)")
 
-	long times[iter];
-	int errors[iter];
-	srand(time(NULL));
-	for(i = 0; i < iter; i++) {
 	
-		/*
-			Enqueue Write Buffer
-		*/
-		for(j = 0; j < num_data; j++) {
-			in_data[j] = (float)rand()/(float)RAND_MAX;
-			out_data[j] = (float)rand()/(float)RAND_MAX;
-		}
-		ret0 = clEnqueueWriteBuffer(queue, in_data_mem, CL_TRUE, 0, in_data_sz, in_data, 0, NULL, NULL);
-		if(ret0 != CL_SUCCESS) {
-			printf("ERROR in clEnqueueWriteBuffer(): %s\n", oclReturnCodeToString(ret0));
-			return -1;
-		}
-		
-		/*
-			Enqueue N-D Range Kernel
-		*/
-		clFinish(queue);
+	double total_time = 0.0;
+	for(i = 0; i < REPEAT; i++) {
+		printf(".");
+		fflush(stdout);
+
+		ret = clEnqueueWriteBuffer(queue, input0_mem, CL_TRUE, 0, input_sz / 2, &input[0], 0, NULL, NULL);
+		CHECK_RC(ret, "clEnqueueWriteBuffer(input0_mem)")
+		ret = clEnqueueWriteBuffer(queue, input1_mem, CL_TRUE, 0, input_sz / 2, &input[input_len/2], 0, NULL, NULL);
+		CHECK_RC(ret, "clEnqueueWriteBuffer(input1_mem)")
+
+		ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&input0_mem);
+		CHECK_RC(ret, "clSetKernelArg(input0)")
+		ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&input1_mem);
+		CHECK_RC(ret, "clSetKernelArg(input1)")
+		ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&output_mem);
+		CHECK_RC(ret, "clSetKernelArg(output)")
+		ret = clSetKernelArg(kernel, 3, sizeof(unsigned int), (void *)&input_len);
+		CHECK_RC(ret, "clSetKernelArg(input_len)")
+
 		cl_event event;
-		ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &gsz, &lsz, 0, NULL, &event);
-		if(ret != CL_SUCCESS) {
-			printf("ERROR in clEnqueueNDRangeKernel(): %s\n", oclReturnCodeToString(ret));				
-			return -1;
-		}
-		clWaitForEvents(1, &event);
-
-		/* 
-			Get Profiling Info 
-		*/
 		cl_ulong queued, submit, start, end;
+		ret = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global_sz, &local_sz, 0, NULL, &event);
+		CHECK_RC(ret, "clEnqueueNDRangeKernel()")
+		clFinish(queue);
+		ret = clWaitForEvents(1, &event);
+		CHECK_RC(ret, "clWaitForEvents()");
 		ret = oclGetProfilingInfo(&event, &queued, &submit, &start, &end);
-		if(ret != CL_SUCCESS) {
-			printf("ERROR in oclGetProfilingInfo(): %s\n", oclReturnCodeToString(ret));				
-			return -1;
-		}
+		CHECK_RC(ret, "oclGetProfilingInfo()")
 
-		/*
-			Read output and verify
-		*/
-		ret0 = clEnqueueReadBuffer(queue, out_data0_mem, CL_TRUE, 0, out_data_sz, out_data0, 0, NULL, NULL);
-		ret1 = clEnqueueReadBuffer(queue, out_data1_mem, CL_TRUE, 0, out_data_sz, out_data1, 0, NULL, NULL);
-		if(ret0 != CL_SUCCESS | ret1 != CL_SUCCESS) {
-			printf("ERROR in clEnqueueReadBuffer(): %s, %s\n", oclReturnCodeToString(ret0), oclReturnCodeToString(ret1));
-			return -1;
-		}
+		if(i != 0) 
+			total_time += ((double)(end - start)) * 1e-9;
 
-		errors[i] = 0;
-		for(j = 0; j < gsz; j++) {
-			errors[i] += in_data0[j] != out_data0[j];
-			if(check == 1)
-				errors[i] += in_data1[j] != out_data1[j];
-		
-//				printf("%f != %f\t%f!=%f\n", in_data0[j], out_data0[j], in_data1[j], out_data1[j]);
+		float out = 0.0, golden = 0.0;
+		ret = clEnqueueReadBuffer(queue, output_mem, CL_TRUE, 0, output_sz, output, 0, NULL, NULL);
+		CHECK_RC(ret, "clEnqueueWriteBuffer(output_mem)")
+		for(j = 0; j < output_sz / sizeof(float); j++)
+			out += output[j];
+		for(j = 0; j < input_sz / sizeof(float); j++) 
+			golden -= input[j];
+		float error = (out-golden)/golden;
+		if(error > 0.0001) 
+			printf("\t%% error: %f\n", error);
 
-		}
-		
-		times[i] = end - start;
+
 	}
+	printf("\n");
 
-	long tot_time = 0;
-	int tot_error = 0;
-	FILE *f = fopen("test.out", "w");
-	for(i = 0; i < iter; i++) {
-
-		if(i > 0) {
-			tot_time += times[i];
-			tot_error += errors[i];
-			fprintf(f, "%d\n", times[i]);
-		} else {
-			fprintf(f, "%d\n", data_size);
-		}
-		
-	}
-	fprintf(f, "%d\n", tot_error);
-	fclose(f);
-	printf("Iterations:\t%d\n", iter);
-	printf("AVG time:\t%ld ns\n", tot_time / (iter - 1));
-	printf("Total errors:\t%d\n", tot_error);
+	double seconds = total_time / (REPEAT - 1);
+	double operations = global_sz * 2;
+	printf("Time:\t\t%f s\n", seconds);
+	printf("Operations:\t%f G\n", operations / 1024 / 1024/ 1024);
+	printf("Bandwidth:\t%f G/s\n", operations/1024/1024/ 1024/seconds);
 
 
-	printf("======= END reduce.c =======\n");
-	
-	return 0;
 
+
+	free(input);
+	free(output);
 }
-
-/*
-void getStat(long *times, long *avg, long *std) {
-	int i;	
-	long tot = 0;
-
-	
-	for(i = 0; i < iter; i++) {
-		tot += times[i];
-	}
-	*avg = tot / iter;
-
-	long sqtot = 0;
-	for(i = 0; i < ITER; i++) {
-		sqtot += (times[i] - *avg) * (times[i] - *avg);
-	}
-	*std = sqrt(sqtot / ITER);
-		
-}*/
-
-
-
